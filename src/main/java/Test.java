34d9de51b3a2f5a648750ad60b570d061a67f697
RamTsk <ramtsk24685@gmail.com>
2023-06-20 09:52:22 +0530

    Collection API - ArrayList Practice

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,9 +3,9 @@ import com.practice.example.basic.accessSpecifier.package1.ACExample;
 
 public class Test{
     public static void main(String[] args) {
-        ACExample obj = new ACExample();
-        System.out.println(obj.a);
-        obj.m1();
-        System.out.println(obj.str);
+//        ACExample obj = new ACExample();
+//        System.out.println(obj.a);
+//        obj.m1();
+//        System.out.println(obj.str);
     }
 }
---
+++ src/main/java/com/practice/example/collectionAPI/ArrayListPractice.java
@@ -0,0 +1,30 @@
+package com.practice.example.collectionAPI;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<String> obj = new ArrayList<String>(16);
+        obj.add("One");
+        obj.add(1,"Two");
+        obj.add("Three");
+        obj.add("Four");
+        obj.add("Five");
+        obj.add("Six");
+        obj.add("Seven");
+        obj.add(0,"1");
+        obj.remove(1);
+        ArrayList<String> obj2 = new ArrayList<String>(32);
+        obj2.addAll(obj);
+        obj2.add("SecondArrayList");
+
+        obj2.removeAll(obj);
+        for(int i=0;i<obj2.size();i++){
+            System.out.println(obj2.get(i));
+        }
+        for(String str : obj){
+            System.out.println(str);
+        }
+        ArrayList objInt = new ArrayList<Integer>();
+    }
+}
