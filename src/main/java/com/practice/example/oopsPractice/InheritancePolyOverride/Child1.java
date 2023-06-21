f8d8b233b45487efe501d7e7bbe325ab512aebf3
Ruthvik88 <10c123ruthvikk@gmail.com>
2023-06-21 08:52:54 +0530

    TestInterface

--- src/main/java/com/practice/example/oopsPractice/InheritancePolyOverride/Child1.java
+++ src/main/java/com/practice/example/oopsPractice/InheritancePolyOverride/Child1.java
@@ -1,11 +1,13 @@
 package com.practice.example.oopsPractice.InheritancePolyOverride;
 
-public class Child1 extends Parent{
+public class Child1 extends Parent {
     @Override
     void m0() {
         System.out.println("Calling m0 method from child1");
     }
+
     void m10() {
         System.out.println("Calling m0 method from child1");
     }
 }
+
