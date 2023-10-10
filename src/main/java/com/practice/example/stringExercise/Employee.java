60a6c848743109972213e9b36fdfb49817d8f0a7
Ramtsk0611 <ramtsk0611@gmail.com>
2023-10-10 07:57:08 +0530

    Added for core Java

---
+++ src/main/java/com/practice/example/stringExercise/Employee.java
@@ -0,0 +1,19 @@
+//***************************************
+//The instance variable of the class is final i.e. we cannot change the value of it after creating an object.
+//The class is final so we cannot create the subclass.
+//There is no setter methods i.e. we have no option to change the value of the instance variable.
+//These points makes this class as immutable.
+//***************************************
+package com.practice.example.stringExercise;
+public final class Employee
+{
+    final String pancardNumber;
+    public Employee(String pancardNumber)
+    {
+        this.pancardNumber=pancardNumber;
+    }
+    public String getPancardNumber(){
+        return pancardNumber;
+    }
+
+}
